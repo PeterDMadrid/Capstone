22fe13f6a014fc3494e7a303ccbb67abd658421f
Peter Madrid <petermadrid0421@gmail.com>
2024-07-03 19:00:48 +0800

    initial file structure

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+@include('auth/landing')
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Your App Name')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
